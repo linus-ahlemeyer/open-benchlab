1c5d12adaae6f53b661ea29e27977f4d07b63aa6
Linus Ahlemeyer <130595869+linus-ahlemeyer@users.noreply.github.com>
2026-06-21 18:53:32 +0200

    Expose runtime adapters

--- open_benchlab/adapters/__init__.py
+++ open_benchlab/adapters/__init__.py
@@ -1,1 +1,10 @@
-# Runtime adapters.
+from .base import LaunchPlan, RuntimeAdapter
+from .llama_cpp import LlamaCppAdapter
+from .openai_compatible import OpenAICompatibleAdapter
+
+__all__ = [
+    "LaunchPlan",
+    "RuntimeAdapter",
+    "LlamaCppAdapter",
+    "OpenAICompatibleAdapter",
+]
